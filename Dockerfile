61ec72e4479b31efb62dcb226ab4a4425ac84fb8
Waldyr Felix <waldyrfelix@gmail.com>
2019-09-14 15:39:29 -0300

    mudança de porta

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,6 @@ WORKDIR /usr/src/app
 COPY ["package.json", "package-lock.json*", "npm-shrinkwrap.json*", "./"]
 RUN npm install --production --silent && mv node_modules ../
 COPY . .
-EXPOSE 3000
+EXPOSE 80
 # CMD npm run cluster
 CMD npm run monothread
--- loadtest.sh
+++ loadtest.sh
@@ -1,3 +1,8 @@
 ab -n 100000 -c 64 -k http://localhost:3000/ > output_nodejs_one_process.txt
 ab -n 100000 -c 64 -k http://localhost:3000/ > output_nodejs_cluster_process.txt
 ab -n 100000 -c 64 -k http://localhost:3000/ > output_nodejs_nginx_lb.txt
+
+
+
+
+ab -n 100000 -c 64 -k http://140.238.183.198:3000/ > output_nodejs_nginx_lb.txt
