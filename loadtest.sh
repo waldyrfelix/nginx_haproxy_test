35f6e681892664ded26dd6711f7f11fff2162181
Waldyr Felix <waldyrfelix@gmail.com>
2019-09-14 14:15:57 -0300

    ajuste

--- loadtest.sh
+++ loadtest.sh
@@ -1,2 +1,3 @@
 ab -n 100000 -c 64 -k http://localhost:3000/ > output_nodejs_one_process.txt
 ab -n 100000 -c 64 -k http://localhost:3000/ > output_nodejs_cluster_process.txt
+ab -n 100000 -c 64 -k http://localhost:3000/ > output_nodejs_nginx_lb.txt
